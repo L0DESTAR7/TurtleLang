7c8bfaaa2331c592cd08ff86cd9bb681f82ee0bc
L0DESTAR7 <107700184+L0DESTAR7@users.noreply.github.com>
2023-03-31 14:25:39 +0000

    src code for compiler & auxiliary files

---
+++ comp/header.h
@@ -0,0 +1,13 @@
+//
+// Created by WALID on 09/02/2023.
+//
+
+#ifndef COMPILATION_HEADER_H
+
+void command();
+
+#define  TOKEN_CAP 200
+
+#define COMPILATION_HEADER_H
+
+#endif //COMPILATION_HEADER_H
